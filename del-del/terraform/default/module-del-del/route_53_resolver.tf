fbe2bb61add1f4dbcb63dfc67f0ced02668acac8
Javier Tarulla <javier.tarulla@cycloid.io>
2024-02-05 13:25:42 +0000

    Delete Service Catalog: del-del

--- del-del/terraform/default/module-del-del/route_53_resolver.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_route53_resolver_rule_association" "rslvr_autodefined_assoc_vpc_b94ae9c4_internet_resolver" {
-  name             = var.aws_route53_resolver_rule_association_rslvr_autodefined_assoc_vpc_b94ae9c4_internet_resolver_name
-  resolver_rule_id = var.aws_route53_resolver_rule_association_rslvr_autodefined_assoc_vpc_b94ae9c4_internet_resolver_resolver_rule_id
-  vpc_id           = var.aws_route53_resolver_rule_association_rslvr_autodefined_assoc_vpc_b94ae9c4_internet_resolver_vpc_id
-}
-
